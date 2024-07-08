1559ef72527f7052efe9247c8eb5c095a510d087
ignacio liboreiro <ignaliboreiro@gmail.com>
2024-07-08 16:23:55 -0300

    subiendo fotos

--- src/secciones/Maquinas/Maquinas.jsx
+++ src/secciones/Maquinas/Maquinas.jsx
@@ -8,10 +8,10 @@ const Maquinas = () => {
         <>
         <h1 className="titulo-maquinas">Nuestras Maquinas</h1>
         <div className="imagenes">
-            <img src="./public/CamionVerde.jpg" alt="" />
-            <img src="./public/palaFrente.jpg" alt="" />
-            <img src="./public/CamionRojo.jpg" alt="" />
-            <img src="./public/palaatras.jpg" alt="" />
+            <img src="public/CamionVerde.jpg" alt="" />
+            <img src="public/palaFrente.jpg" alt="" />
+            <img src="public/CamionRojo.jpg" alt="" />
+            <img src="public/palaatras.jpg" alt="" />
         </div>
         </>
     );
